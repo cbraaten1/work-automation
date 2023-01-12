82ef97053e3e2eeabbd364322e22e1608e59a94b
cbraaten1 <95046227+cbraaten1@users.noreply.github.com>
2023-01-12 15:37:35 -0600

    reimage_asset_script
    This script is used to read an xlsx file that writes to a text file with any IT assets that need to be reimaged for redeployment. After the file has been written, the script will send an email to all involved parties with the text file as an attachment.

---
+++ reimage_asset_script.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import win32com.client as win32
+
+xlsx = r'A\path\to\the\xlsx\file' # Enter file path for the xlsx document.
+
+df = pd.read_excel(xlsx, sheet_name='some sheet name') # Enter the sheet name from the xlsx file.
+df = df[df['some column header'].notna()] # Pulls the value (number) from the column if applicable.
+df.to_csv('csv_to.csv', index=False)
+
+read_csv = pd.read_csv('csv_to.csv')
+# dtype_before = type(test['hold number']) # Getting the type.
+asset_list = read_csv['asset'].tolist()
+hold_list = read_csv['hold number'].tolist()
+# dtype_after = type(hold_list) # Getting the type.
+# print('Data type before converting = {}\nData type after converting = {}'.format(dtype_before,dtype_after)) # Showing the conversion to a list.
+to_dict = dict(zip(asset_list, hold_list))
+
+asset_less_zero = []
+
+for asset, laptop in to_dict.items():
+    try:
+        value = int(laptop)
+    except ValueError:
+        continue    
+        
+    if value < 0:    
+        asset_less_zero.append(asset)
+
+email_file = open(r'A\path\to\the\xlsx\file', 'r+')
+email_file.truncate(0)
+email_file.write(str(asset_less_zero))
+email_file.close()
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'Enter email address here'
+mail.Subject = 'A random subject title'
+mail.Body = ''
+
+attachment  = r'A\path\to\the\txt\file'
+mail.Attachments.Add(attachment)
+
+mail.Send()
