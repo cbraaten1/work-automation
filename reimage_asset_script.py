0706010a8a8f89dca816f01ee0a208522862bbff
cbraaten1 <95046227+cbraaten1@users.noreply.github.com>
2023-01-13 11:53:47 -0600

    reimage_asset_script.py

--- reimage_asset_script.py
+++ reimage_asset_script.py
@@ -4,13 +4,13 @@ import win32com.client as win32
 xlsx = r'A\path\to\the\xlsx\file' # Enter file path for the xlsx document.
 
 df = pd.read_excel(xlsx, sheet_name='some sheet name') # Enter the sheet name from the xlsx file.
-df = df[df['some column header'].notna()] # Pulls the value (number) from the column if applicable.
+df = df[df['enter column header'].notna()] # Pulls the value (number) from the column if applicable.
 df.to_csv('csv_to.csv', index=False)
 
 read_csv = pd.read_csv('csv_to.csv')
-# dtype_before = type(test['hold number']) # Getting the type.
-asset_list = read_csv['asset'].tolist()
-hold_list = read_csv['hold number'].tolist()
+# dtype_before = type(test['enter column header']) # Getting the type.
+asset_list = read_csv['enter column header'].tolist()
+hold_list = read_csv['enter column header'].tolist()
 # dtype_after = type(hold_list) # Getting the type.
 # print('Data type before converting = {}\nData type after converting = {}'.format(dtype_before,dtype_after)) # Showing the conversion to a list.
 to_dict = dict(zip(asset_list, hold_list))
@@ -34,8 +34,8 @@ email_file.close()
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
 mail.To = 'Enter email address here'
-mail.Subject = 'A random subject title'
-mail.Body = ''
+mail.Subject = 'Enter subject title here'
+mail.Body = 'Enter the body of the email here'
 
 attachment  = r'A\path\to\the\txt\file'
 mail.Attachments.Add(attachment)
