28a16a6c0d29bcc44692f7165c3449981cd62267
Connor Braaten <95046227+cbraaten1@users.noreply.github.com>
2023-02-22 21:02:49 -0600

    computer_setup.py
    Automating the standard software programs that a user needs to complete tasks for their job.

--- computer_setup.py
+++ computer_setup.py
@@ -1,10 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+# import os
+import schedule
+import time
 
-driver = webdriver.Chrome()
+def test():
+    print('Hello World')
+    return schedule.CancelJob
 
-driver.get('https://www.google.com/chrome/')
+def test2():
+    print('This might work!')
+    return schedule.CancelJob
 
-folder = driver.find_element('xpath', '//*[@id="js-download-hero"]')
+def test3():
+    print('This is the 3rd test!')
+    return schedule.CancelJob
 
-# https://learn.microsoft.com/en-us/microsoft-edge/webdriver-chromium/?tabs=python
+schedule.every(1).seconds.do(test)
+schedule.every(1).minute.do(test2)
+schedule.every(65).seconds.do(test3)
+
+while 1:
+    n = schedule.idle_seconds()
+
+    if n is None:
+        # no more jobs
+        break
+    elif n > 0:
+        # sleep exactly the right amount of time.
+        time.sleep(n)
+    schedule.run_pending()
+
+# Internet Documentation:
+#   https://www.geeksforgeeks.org/python-script-that-is-executed-every-5-minutes/ - Original idea on how to execute code on a timer; see module #2.
+#   https://schedule.readthedocs.io/en/stable/examples.html - The while loop is pulling from "Time Until Next Execution".
